b40fb6bd1b5672c7ff704eb0104ab646af19b913
CHO <202430312@email.daelim.ac.kr>
2024-11-20 16:46:32 +0900

    ...

---
+++ src/TCPServer.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+
+public class TCPServer {
+    public static void main(String[] args) throws Exception {
+        String myIp = new MyIp().getLocalIp();
+        ServerSocket serverSocket = new ServerSocket(13224);
+        System.out.println("Server is running on " + myIp + ":8888 ....");
+
+        Socket socket = serverSocket.accept();
+
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        String clientMessage = in.readLine();
+        System.out.println("Client: " + clientMessage);
+
+        out.println("Hello, Client!");
+        socket.close();
+        serverSocket.close();
+    }
+}
